1c55190a6c4f7dd3cd97ee8432903e4be2cb0845
Guilherme Leite da Silva <109865029+GLMLeite@users.noreply.github.com>
2023-03-17 11:49:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.status(200).send({ message: "Ola Mundo" });
 });
+app.get("/ola", (req, res) => {
+  res.status(200).send({ message: "oi sou a rota ola" });
+});
 app.listen(3001, () => {
   console.log("api rodando na porta 3001");
 });
