1ad4cb03eaaee38c7efad40ac05e334cf091ca40
Guilherme Leite <guilhermeleitedasilva9@gmail.com>
2023-03-17 11:42:29 -0300

    criado o servidor api com express

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send({ message: "Ola Mundo" });
+});
+app.listen(3001, () => {
+  console.log("api rodando na porta 3001");
+});
